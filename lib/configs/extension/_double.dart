42ce622de481874da17b26aaa550561d59f41de4
Syed Shayan <139269261+3SyedShayan@users.noreply.github.com>
2026-07-21 22:38:53 +0500

    feat: implement scalable spacing and layout utility system with custom tokens and extensions

---
+++ lib/configs/extension/_double.dart
@@ -0,0 +1,13 @@
+part of '../configs.dart';
+
+extension SuperDouble on double {
+  double un() => AppUnit.un(this);
+  double sp() => AppUnit.sp(this);
+  double font() => AppUnit.font(this);
+  // Syntactic Sugar
+  Widget h() => SizedBox(height: this);
+  Widget w() => SizedBox(width: this);
+  Widget box() => SizedBox(height: this, width: this);
+
+  BorderRadius radius() => BorderRadius.circular(this);
+}
